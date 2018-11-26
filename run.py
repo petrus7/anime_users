e332c70e50c9e2c120f446842c7ef86c0d28987a
Piotr Dankowski <piotr.dankowski@autea.eu>
2018-11-26 13:40:18 +0100

    project setup

---
+++ run.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == '__main__':
+    print('ANIME LIST USERS')
+    print('ANIME LIST USERS')
+	print('ANIME LIST USERS')
+	print('ANIME LIST USERS')
+	print('ANIME LIST USERS')
+	print('ANIME LIST USERS')
