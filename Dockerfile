3f997f54cead25bf5bfeea50993c6fdaff50e2ae
Piotr Dankowski <piotr.dankowski@autea.eu>
2018-11-26 21:00:17 +0100

    connect to database, create diagnostic endpoint

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["python", "run.py"]
+CMD ["flask", "run", "--host=0.0.0.0"]
--- run.py
+++ run.py
@@ -1,9 +1,15 @@
+from flask import Flask
+
+from src.anime_list_service import create_app
 
 
-if __name__ == '__main__':
-    print('ANIME LIST USERS')
-    print('ANIME LIST USERS')
-	print('ANIME LIST USERS')
-	print('ANIME LIST USERS')
-	print('ANIME LIST USERS')
-	print('ANIME LIST USERS')
+
+app = create_app('DevelopmentConfig')
+
+@app.route('/')
+def test():
+    return 'ala nie ma kota'
+
+
+# if __name__ == '__main__':
+#     app.run()
